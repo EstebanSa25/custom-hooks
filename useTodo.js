300da523667551acc7ebfa3042faa09701916b28
EstebanSa25 <estebansalas2502@gmail.com>
2023-11-01 15:36:10 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,46 @@
+import {useEffect, useReducer} from "react";
+import {TodoReducer} from "../08-useReducer/todoReducer.js";
+
+export const UseTodo = () => {
+    //#region ParametersReducer
+    const init = () => {
+        return JSON.parse(localStorage.getItem('todos')) || [];
+    }
+    //#endregion
+    //#region Hooks
+    const [todos, dispatch] = useReducer(TodoReducer, [], init)
+
+    useEffect(() => {
+        console.log(todos)
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+    //#endregion
+    //#region fnEventos
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove todo',
+            payload: id
+        })
+    }
+    const handleToggleTodo = (id) => {
+        console.log({id});
+        dispatch({
+            type: '[TODO] Toggle todo',
+            payload: id
+        })
+    }
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add todo',
+            payload: todo
+        }
+        dispatch(action);
+    }
+    const todosCount = () => (todos.length)
+    const pendingTodosCount = () => (todos.filter(todo => !todo.done).length)
+    //#endregion
+
+    return {
+        todos, handleNewTodo, handleDeleteTodo, handleToggleTodo, todosCount, pendingTodosCount
+    }
+}
